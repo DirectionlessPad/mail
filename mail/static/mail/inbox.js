dab575326a4436a8988f0a07ce68d0658169f533
DirectionlessPad <patrickmoore937@gmail.com>
2024-03-11 21:23:33 +0000

    Implemented compose email and basic mailbox display.

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -11,15 +11,34 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 function compose_email() {
-
+  
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
-
+  
   // Clear out composition fields
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
+  
+  // Add event listener to submit button
+  document.querySelector('form').onsubmit = () => {
+    fetch('/emails', {
+      method: 'POST',
+      body: JSON.stringify({
+          recipients: document.querySelector('#compose-recipients').value,
+          subject: document.querySelector('#compose-subject').value,
+          body: document.querySelector('#compose-body').value,
+      })
+    })
+    // .then(response => response.json())
+    // .then(result => {
+    //     // Print result
+    //     console.log(result);
+    // });
+    document.querySelector('#sent').click();
+    return false;
+  }
 }
 
 function load_mailbox(mailbox) {
@@ -30,4 +49,24 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+  // Get the emails
+  fetch(`/emails/${mailbox}`)
+    .then(response => response.json())
+    .then(emails => {
+        // Print emails
+        console.log(emails);
+        emails.forEach((email) => {
+          const display_email = document.createElement('div');
+          display_email.classList.add('email-preview')
+          if (mailbox === 'sent') {
+            who = `To: ${email.recipients}`
+          } else {
+            who = `From: ${email.sender}`
+          }
+          display_email.innerHTML = `<h4>${email.subject}</h4><p>${who}</p><p>${email.body}</p>`;
+          document.querySelector('#emails-view').append(display_email);
+        });
+    });
+  
 }
