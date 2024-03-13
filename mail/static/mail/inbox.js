13ec06684f2bde5a166d6ddf913bc245d00dd3a2
DirectionlessPad <patrickmoore937@gmail.com>
2024-03-13 18:46:25 +0000

    Implemented email archiving

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -1,26 +1,26 @@
 document.addEventListener('DOMContentLoaded', function() {
 
   // Use buttons to toggle between views
-  document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
-  document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
-  document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
-  document.querySelector('#compose').addEventListener('click', compose_email);
+  document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'))
+  document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'))
+  document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'))
+  document.querySelector('#compose').addEventListener('click', compose_email)
 
   // By default, load the inbox
-  load_mailbox('inbox');
-});
+  load_mailbox('inbox')
+})
 
 function compose_email() {
   
   // Show compose view and hide other views
-  document.querySelector('#mailbox-view').style.display = 'none';
-  document.querySelector('#email-view').style.display = 'none';
-  document.querySelector('#compose-view').style.display = 'block';
+  document.querySelector('#mailbox-view').style.display = 'none'
+  document.querySelector('#email-view').style.display = 'none'
+  document.querySelector('#compose-view').style.display = 'block'
   
   // Clear out composition fields
-  document.querySelector('#compose-recipients').value = '';
-  document.querySelector('#compose-subject').value = '';
-  document.querySelector('#compose-body').value = '';
+  document.querySelector('#compose-recipients').value = ''
+  document.querySelector('#compose-subject').value = ''
+  document.querySelector('#compose-body').value = ''
   
   // Send email
   document.querySelector('form').onsubmit = () => {
@@ -34,22 +34,22 @@ function compose_email() {
     })
 
     // Navigate to 'Sent' mailbox
-    document.querySelector('#sent').click();
+    document.querySelector('#sent').click()
 
     // Add 'return false' to avoid reloading the page when the form is submitted
-    return false;
+    return false
   }
 }
 
 function load_mailbox(mailbox) {
   
   // Show the mailbox and hide other views
-  document.querySelector('#mailbox-view').style.display = 'block';
-  document.querySelector('#email-view').style.display = 'none';
-  document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector('#mailbox-view').style.display = 'block'
+  document.querySelector('#email-view').style.display = 'none'
+  document.querySelector('#compose-view').style.display = 'none'
 
   // Show the mailbox name
-  document.querySelector('#mailbox-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+  document.querySelector('#mailbox-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`
 
   // Get the emails
   fetch(`/emails/${mailbox}`)
@@ -60,25 +60,25 @@ function load_mailbox(mailbox) {
       emails.forEach((email) => {
         const preview_email = createCustomElement('div','email-preview')
         if (email.read) {
-          preview_email.classList.add('read');
+          preview_email.classList.add('read')
         } else {
-          preview_email.classList.add('unread');
+          preview_email.classList.add('unread')
         }
 
         // Display email address based on if the user is the sender or receiver
         if (mailbox === 'sent') {
-          who = `To: ${email.recipients}`;
+          who = `To: ${email.recipients}`
         } else {
-          who = `From: ${email.sender}`;
+          who = `From: ${email.sender}`
         }
 
         // Create HTML for the email preview
         const preview_container = createCustomElement('div', 'container')
         const preview_row = createCustomElement('div', 'row')
         
-        who = `<div class="col 4">${who}</div>`;
-        subject = `<div class="col 4">${email.subject}</div>`;
-        timestamp = `<div class="col 4" style="text-align:right;">${email.timestamp}</div>`;
+        who = `<div class="col 4">${who}</div>`
+        subject = `<div class="col 4">${email.subject}</div>`
+        timestamp = `<div class="col 4" style="text-align:right">${email.timestamp}</div>`
         preview_row.innerHTML = `${who}${subject}${timestamp}`
         preview_container.append(preview_row)
         preview_email.append(preview_container)
@@ -86,18 +86,18 @@ function load_mailbox(mailbox) {
         // If the email is clicked then go to detailed email view
         preview_email.addEventListener('click', () => {
           load_email(email.id)
-        });
-        document.querySelector('#mailbox-view').append(preview_email);
-      });
-    }); 
+        })
+        document.querySelector('#mailbox-view').append(preview_email)
+      })
+    }) 
 }
 
 function load_email(email_id) {
 
   // Show the email and hide other views
-  document.querySelector('#mailbox-view').style.display = 'none';
-  document.querySelector('#email-view').style.display = 'block';
-  document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector('#mailbox-view').style.display = 'none'
+  document.querySelector('#email-view').style.display = 'block'
+  document.querySelector('#compose-view').style.display = 'none'
 
   // Display detailed email view
   fetch(`/emails/${email_id}`)
@@ -108,8 +108,28 @@ function load_email(email_id) {
       subject = `<b>Subject:</b> ${email.subject}`
       timestamp = `<b>Timestamp:</b> ${email.timestamp}`
       body = `<p>${email.body}</p>`
-      document.querySelector('#email-view').innerHTML = `${sender}<br>${receivers}<br>${subject}<br>${timestamp}<hr>${body}`
-    });
+      document.querySelector('#email-view').innerHTML = `${sender}<br>${receivers}<br>${subject}<br>${timestamp}<hr>${body}<hr>`
+      
+      // Create button for archiving/unarchiving the email
+      const archive_btn = document.createElement('button')
+      document.querySelector('#email-view').append(archive_btn)
+      if (email.archived) {
+        archive_btn.innerHTML = "Unarchive"
+      } else {
+        archive_btn.innerHTML = "Archive"
+      }
+      archive_btn.addEventListener('click', () => {
+        fetch(`/emails/${email_id}`, {
+          method: 'PUT',
+          body: JSON.stringify({
+              archived: !email.archived
+          })
+        })
+
+        // Reloading the page will return the user to the inbox view
+        location.reload()
+      })
+    })
   
   // Update read property upon opening the email
   fetch(`/emails/${email_id}`, {
@@ -118,16 +138,15 @@ function load_email(email_id) {
         read: true
     })
   })
-  return false;
 }
 
 function createCustomElement(tag, classes) {
-  classes = [].concat(classes);
-  element = document.createElement(tag);
+  classes = [].concat(classes)
+  element = document.createElement(tag)
   if (classes) {
     classes.forEach((htmlclass) => {
-      element.classList.add(htmlclass);
+      element.classList.add(htmlclass)
     })
   }
-  return element;
+  return element
 }
